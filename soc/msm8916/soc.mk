18ab2029fb4467f3f6d0c4c2bf81c0ded1533058
Ralph Nathan <ralphnathan@google.com>
2015-09-14 11:38:46 -0700

    audio: Create directory for prebuilts for audio.
    These libraries and daemons are required for the kernel to detect
    audio devices in /dev/snd for msm8916.
    
    BUG=none
    TEST=sound devices are detected in /dev/snd on the db410c
    
    Change-Id: If20ac650d33d1ca55063a6e134e63118d955cb43

--- soc/msm8916/soc.mk
+++ soc/msm8916/soc.mk
@@ -42,8 +42,33 @@ PRODUCT_COPY_FILES += \
 # TODO: Move common/prebuilts/sepolicy include to audio_prebuilts.mk
 BOARD_SEPOLICY_DIRS += \
 	$(LOCAL_PATH)/sepolicy \
-	$(TOP)/hardware/bsp/qcom/common/prebuilts/sepolicy
+	$(LOCAL_PATH)/prebuilts/sepolicy \
 
 # Set up the local kernel.
 TARGET_KERNEL_SRC := hardware/bsp/kernel/qcom/android-msm-3.10
 TARGET_KERNEL_DEFCONFIG := dragonboard_defconfig
+
+# Include prebuilts to detect audio devices.
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/prebuilts/init.audio.rc:system/etc/init/init.audio.rc \
+
+# Audio daemons
+PRODUCT_COPY_FILES += \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/daemons/rmt_storage:/system/bin/rmt_storage \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/daemons/qmuxd:/system/bin/qmuxd \
+
+# Audio libs
+PRODUCT_COPY_FILES += \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmi_common_so.so:/system/lib/libqmi_common_so.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmi_csi.so:/system/lib/libqmi_csi.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libsmemlog.so:/system/lib/libsmemlog.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmi_encdec.so:/system/lib/libqmi_encdec.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libconfigdb.so:/system/lib/libconfigdb.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libdiag.so:/system/lib/libdiag.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libdsutils.so:/system/lib/libdsutils.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmi.so:/system/lib/libqmi.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libxml.so:/system/lib/libxml.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libidl.so:/system/lib/libidl.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libmdmdetect.so:/system/lib/libmdmdetect.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmi_client_qmux.so:/system/lib/libqmi_client_qmux.so \
+	$(TOP)/hardware/bsp/qcom/soc/msm8916/prebuilts/libs/libqmiservices.so:/system/lib/libqmiservices.so \
